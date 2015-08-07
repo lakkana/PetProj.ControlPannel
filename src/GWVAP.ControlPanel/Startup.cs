6b519e1bf0162e862b4b7dde88a27882992862e4
lakkana <email2lakkana@gmail.com>
2015-08-07 16:35:29 +0100

    Adding the initial wireframe

---
+++ src/GWVAP.ControlPanel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GWVAP.ControlPanel.Startup))]
+namespace GWVAP.ControlPanel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
